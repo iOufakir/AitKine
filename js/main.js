0aef3153e2744de5111046053873b881a209508c
iLyas <ouf.ilyas@gmail.com>
2022-04-24 22:02:20 +0000

    Add about page

--- js/main.js
+++ js/main.js
@@ -168,10 +168,10 @@
 	var ssAOS = function() {
 
 		AOS.init( {
-      	offset: 200,
-      	duration: 600,
+      	offset: 100,
+      	duration: 400,
       	easing: 'ease-in-sine',
-      	delay: 300,
+      	delay: 200,
 			once: true,
 			disable: 'mobile'
     	});
